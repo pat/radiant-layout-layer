cc191d11dc1fb888556b0c3d2d5a1f8517da61a9
Pat Allan <pat@freelancing-gods.com>
2009-01-23 10:17:58 +0700

    Adding snippets support as well

--- layout_layer_extension.rb
+++ layout_layer_extension.rb
@@ -2,8 +2,8 @@ require 'action_controller/dispatcher'
 require 'layout_layer'
 
 class LayoutLayerExtension < Radiant::Extension
-  version "1.0.1"
-  description "Automatically create layouts based on HTML files in other extensions"
+  version "1.1"
+  description "Automatically create layouts and snippets based on HTML files in other extensions"
   url "http://github.com/freelancing-god/radiant-layout-layer"
   
   def activate
--- lib/layout_layer.rb
+++ lib/layout_layer.rb
@@ -1,9 +1,10 @@
 class LayoutLayer
-  LayoutsPath = File.join RAILS_ROOT, "vendor", "extensions", "*", "layouts"
+  LayoutsPath  = File.join RAILS_ROOT, "vendor", "extensions", "*", "layouts"
+  SnippetsPath = File.join RAILS_ROOT, "vendor", "extensions", "*", "snippets"
   
   def self.lay
     ActionController::Base.logger.add(
-      Logger::DEBUG, "\n  LayoutLayer: Laying down some layouts"
+      Logger::DEBUG, "\n  LayoutLayer: Laying down some layouts and snippets"
     )
     
     Dir[LayoutsPath].each do |layouts|
@@ -13,6 +14,16 @@ class LayoutLayer
         
         layout.content = open(html) { |file| file.read }
         layout.save
+      end
+    end
+    
+    Dir[SnippetsPath].each do |snippets|
+      Dir[File.join(snippets, "**", "*.html")].each do |html|
+        name = file_name_to_layout_name(snippets, html)
+        snippet = Snippet.find_by_name(name) || Snippet.new(:name => name)
+        
+        snippet.content = open(html) { |file| file.read }
+        snippet.save
       end
     end
   end
